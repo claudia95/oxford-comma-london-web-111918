05e28415faaa83de0526984e16dca1ae82428f1a
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 00:08:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
-  array.join()
+  array.join
   elsif array.length == 2 
     array.join(" and ")
+  elsif array.length == 3 
+  
     
   end
 end
